ff9bdc8a92440759a1e1748ad36b73700d21b1eb
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-12-26 18:53:42 -0500

    log webhook data to cloudwatch

--- iac_cost_estimator/app.py
+++ iac_cost_estimator/app.py
@@ -1,6 +1,16 @@
 import json
 
+
 # log the event and return the event in the response
 def lambda_handler(event, context):
-    print(json.dumps(event))
+    print(json.dumps(event, indent=4))
+
+    if "body" in event and event["body"] is not None:
+        try:
+            body_obj = json.loads(event["body"])
+            print("Pretty-printed body:", json.dumps(body_obj, indent=4))
+
+        except json.JSONDecodeError:
+            print("Error decoding JSON")
+
     return {"statusCode": 200, "body": json.dumps(event)}
