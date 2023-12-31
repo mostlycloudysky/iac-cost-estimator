956e506b43a4811904695b5c4c568ffb312189a7
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2023-12-31 09:45:33 -0500

    add code to find the iactype

--- determine_iac_type/app.py
+++ determine_iac_type/app.py
@@ -4,5 +4,13 @@ import json
 # Return cloudformation or terraform
 def lambda_handler(event, context):
     # TODO implement
-    iac_type = "cloudformation"
-    return {"iacType": iac_type}
+    default_iac_type = "unknown"
+
+    if event.get("changes") and len(event["changes"]) > 0:
+        first_change = event["changes"][0]
+        iac_type = first_change.get("file_type", default_iac_type)
+    else:
+        iac_type = default_iac_type
+
+    event["iacType"] = iac_type
+    return event
